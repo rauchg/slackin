31b5b8c36cab7038315ce4e2090ab2d6c3a0d6fa
Aidan Feldman <aidan.feldman@gsa.gov>
2015-04-29 01:03:35 -0400

    add test around a successful request

--- test/index.js
+++ test/index.js
@@ -1,4 +1,38 @@
+import nock from 'nock';
+import request from 'supertest';
+import slackin from '../lib/index';
 
-describe('', () => {
+describe('slackin', () => {
+  describe('POST /invite', () => {
+    it("returns success for a successful invite", (done) => {
+      let opts = {
+        token: 'mytoken',
+        org: 'myorg'
+      };
 
+      // TODO simplify mocking
+      nock(`https://${opts.org}.slack.com`)
+        .get('/api/rtm.start?token=mytoken')
+        .reply(200, {
+          team: {
+            name: 'myteam',
+            icon: {}
+          },
+          users: [{}]
+        });
+
+      nock(`https://${opts.org}.slack.com`)
+        .post('/api/users.admin.invite')
+        .reply(200, { ok: true });
+
+      let app = slackin(opts);
+
+      request(app)
+        .post('/invite')
+        .send({ email: 'foo@example.com' })
+        // .expect('Content-Type', /json/)
+        .expect(200, {})
+        .end(done);
+    });
+  });
 });
