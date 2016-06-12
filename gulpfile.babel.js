7c756b8d80a242f91e292b611795814e5a4b86dc
Leonard Lamprecht <mindrun@icloud.com>
2016-06-12 20:04:17 +0200

    Proper name for build output

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,25 +3,29 @@ import babel from 'gulp-babel'
 import rimraf from 'gulp-rimraf'
 
 const paths = {
-  in_js: 'lib/*.js',
-  in_assets: 'lib/assets/*',
-  out_js: 'node',
-  out_assets: 'node/assets',
+  in: {
+    js: 'lib/*.js',
+    assets: 'lib/assets/*'
+  },
+  out: {
+    js: 'dist',
+    assets: 'dist/assets'
+  }
 }
 
 gulp.task('transpile', () => {
-  return gulp.src(paths.in_js)
+  return gulp.src(paths.in.js)
   .pipe(babel())
-  .pipe(gulp.dest(paths.out_js))
+  .pipe(gulp.dest(paths.out.js))
 })
 
 gulp.task('assets', () => {
-  return gulp.src(paths.in_assets)
-  .pipe(gulp.dest(paths.out_assets))
+  return gulp.src(paths.in.assets)
+  .pipe(gulp.dest(paths.out.assets))
 })
 
 gulp.task('clean', () => {
-  return gulp.src(paths.out_js, {
+  return gulp.src(paths.out.js, {
     read: false
   })
   .pipe(rimraf())
