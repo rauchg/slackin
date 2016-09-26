a506f2fd33e6242e73e43cf1c064e4cca8437c1e
Arun Thampi <arun.thampi@gmail.com>
2016-09-25 18:18:36 -0700

    Supports letsencrypt challenges (#239)
    So you can setup LetsEncrypt certificates natively

--- test/index.js
+++ test/index.js
@@ -74,4 +74,47 @@ describe('slackin', () => {
         .end(done);
     });
   });
+
+  describe('GET /.well-known/acme-challenge/:id', () => {
+    beforeEach(() => {
+      process.env.LETSENCRYPT_CHALLENGE = 'letsencrypt-challenge';
+
+      nock('https://myorg.slack.com')
+        .get('/api/users.list')
+        .query({token: 'mytoken', presence: '1'})
+        .query({token: 'mytoken'})
+        .reply(200, {
+          ok: true,
+          members: [{}]
+        });
+
+      nock('https://myorg.slack.com')
+        .get('/api/channels.list?token=mytoken')
+        .reply(200, {
+          ok: true,
+          channels: [{}]
+        });
+
+      nock('https://myorg.slack.com')
+        .get('/api/team.info?token=mytoken')
+        .reply(200, {
+          ok: true,
+          team: {icon: {}}
+        })
+    });
+
+    it('returns the contents of the environment variable LETSENCRYPT_CHALLENGE', (done) => {
+      let opts = {
+        token: 'mytoken',
+        org: 'myorg'
+      };
+
+      let app = slackin(opts);
+
+      request(app)
+        .get('/.well-known/acme-challenge/deadbeef')
+        .expect(200, 'letsencrypt-challenge')
+        .end(done);
+    })
+  });
 });
