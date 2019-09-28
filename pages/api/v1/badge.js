a58dca5537fa850287ec4eb14ffc7b9f02f51db6
Luis Alvarez <luisito453@gmail.com>
2019-09-27 19:14:12 -0500

    Added badge endpoint

---
+++ pages/api/v1/badge.js
@@ -0,0 +1,53 @@
+import svg from 'vd'
+import { getTotalUsers } from './users'
+
+const title = 'slack'
+const color = '#E01563'
+const pad = 8 // left / right padding
+const sep = 4 // middle separation
+
+function badgeSvg({ total, active }) {
+  let value = active ? `${active}/${total}` : '' + total || '–'
+  let lw = pad + width(title) + sep // left side width
+  let rw = sep + width(value) + pad // right side width
+  let tw = lw + rw // total width
+
+  return svg(
+    `svg xmlns="http://www.w3.org/2000/svg" width=${tw} height=20`,
+    svg(`rect rx=3 width=${tw} height=20 fill=#555`),
+    svg(`rect rx=3 x=${lw} width=${rw} height=20 fill=${color}`),
+    svg(`path d="M${lw} 0h${sep}v20h-${sep}z" fill=${color}`),
+    svg(
+      'g text-anchor=middle font-family=Verdana font-size=11',
+      text({ str: title, x: Math.round(lw / 2), y: 14 }),
+      text({ str: value, x: lw + Math.round(rw / 2), y: 14 })
+    )
+  )
+}
+
+// generate text with 1px shadow
+function text({ str, x, y }) {
+  return [
+    svg(`text fill=#010101 fill-opacity=.3 x=${x} y=${y + 1}`, str),
+    svg(`text fill=#fff x=${x} y=${y}`, str),
+  ]
+}
+
+// π=3
+function width(str) {
+  return 7 * str.length
+}
+
+export default async function badge(req, res) {
+  try {
+    const users = await getTotalUsers()
+
+    res.setHeader('Content-Type', 'image/svg+xml')
+    // Cache this response, this same response will be used for all users thanks to SPR
+    res.setHeader('cache-control', 's-maxage=4,stale-while-revalidate')
+    res.send(badgeSvg(users).toHTML())
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: error.message })
+  }
+}
--- pages/api/v1/users.js
+++ pages/api/v1/users.js
@@ -1,46 +1,48 @@
 import slack from '../../../utils/api/slack'
+
+export async function getTotalUsers() {
+  // The client has a poller to this endpoint, it can quickly exceed the rate
+  // limit in Slack: Tier 2 (20+ per minute)
+  if (process.env.NODE_ENV !== 'production') {
+    return {
+      total: Math.round(Math.random()) > 0 ? 630 : 635,
+      active: Math.floor(Math.random() * (100 - 60)) + 60,
+    }
+  }
+
+  const { ok, members } = await slack.users.list({ presence: true })
+
+  if (!ok) {
+    throw new Error('An unexpected error occurred with Slack')
+  }
+
+  let total = 0
+  let active = 0
+
+  members.forEach(m => {
+    // Remove Slackbot and bots from the members.
+    // Slackbot is not a bot, go figure.
+    if (m.id === 'USLACKBOT' || m.is_bot || m.deleted) return
+
+    total += 1
+    if (m.presence === 'active') {
+      active += 1
+    }
+  })
+
+  return { total, active }
+}
 /**
  * Returns a count of total and active users
  * https://api.slack.com/methods/users.list
  */
 export default async function users(req, res) {
   try {
-    // The client has a poller to this endpoint, it can quickly exceed the rate
-    // limit in Slack: Tier 2 (20+ per minute)
-    if (process.env.NODE_ENV !== 'production') {
-      res.json({
-        users: {
-          total: Math.round(Math.random()) > 0 ? 630 : 635,
-          active: Math.floor(Math.random() * (100 - 60)) + 60,
-        },
-      })
-      return
-    }
-
-    const { ok, members } = await slack.users.list({ presence: true })
-
-    if (!ok) {
-      throw new Error('An unexpected error occurred with Slack')
-    }
-
-    let total = 0
-    let active = 0
-
-    members.forEach(m => {
-      // Remove Slackbot and bots from the members.
-      // Slackbot is not a bot, go figure.
-      if (m.id === 'USLACKBOT' || m.is_bot || m.deleted) return
-
-      total += 1
-      if (m.presence === 'active') {
-        active += 1
-      }
-    })
+    const users = await getTotalUsers()
 
     // Cache this response, this same response will be used for all users thanks to SPR
     res.setHeader('cache-control', 's-maxage=4,stale-while-revalidate')
-
-    res.json({ users: { total, active } })
+    res.json({ users })
   } catch (error) {
     console.error(error)
     res.status(500).json({ error: error.message })
